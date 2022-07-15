b9e59abf8deb1c83f29ae4211ce92e02fe661919
Dmitry Papkov <papkovdmitry@gmail.com>
2022-07-15 23:04:56 +0500

    basic session storage

--- plugins/auth.js
+++ plugins/auth.js
@@ -1,6 +1,9 @@
 import fstifyCookie from "@fastify/cookie"
 import fastifySession from "@fastify/session"
+import store from "connect-mongo"
 import fp from "fastify-plugin"
+
+const MongoStore = new store({ mongoUrl: process.env.CONNECT_DB })
 
 async function auth(fastify, opts) {
   await fastify.register(fstifyCookie)
@@ -10,6 +13,7 @@ async function auth(fastify, opts) {
     secret: fastify.config.SALT,
     cookie: { secure: false },
     maxAge: 3600 * 8,
+    store: MongoStore,
   })
 
   // Декоратор для проверки авторизации в маршрутах.
